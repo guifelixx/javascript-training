f277c944a70c945103639b797651f4bec27e1776
Guilherme Felix Pascoal <142191577+guifelixx@users.noreply.github.com>
2024-06-07 05:03:37 -0300

    Iniciando minha jornada!

---
+++ aula1/ex01.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Guilherme". Estou aprendendo JavaScript às', 4,'da manhã');
---
+++ aula1/script.js
@@ -0,0 +1,15 @@
+//função para imprimir variaveis, literais e objetos (Como o "print" do Python)
+console.log('Hello World!');
+
+console.log('Guilherme Felix Pascoal'); //string
+console.log("Estou aprendendo JavaScript"); //string
+console.log(`A Jéssica é linda`); //string
+
+
+//Não existe separação de int e float, em JavaScript é tudo number
+console.log(123456); //int = number
+console.log(1, 10, 100, 1000); //int = number
+console.log(1.5, 7.5, 10.50); //float = number
+
+console.log(true); //boolean
+console.log(false); //boolean
