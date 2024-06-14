906002f3298c2a01d2682749a1ef0d35ec67617d
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-06-14 04:46:45 -0300

    dados primitivos

---
+++ aula3/difrença.js
@@ -0,0 +1,11 @@
+//ECMAScript 2015 (ES6) padronização da linguagem = JavaScript
+
+nome = 'Luiz'; //NÃO FAZER ISSO
+
+var nome = 'Guilherme'; //Com var podemos redeclarar a váriavel, essa é a diferença de var e let
+
+let nome = 'Guilherme'; //Não conseguimos redeclarar
+let nome = 'Felix'; //Não conseguimos redeclarar
+
+
+/* Regra: Sempre criar variáveis com let e consts SEMPRE!!! */
---
+++ aula4/dados-primitivos.js
@@ -0,0 +1,16 @@
+//String, number, undefined
+
+const nome = 'Guilherme'; //String
+const nome1 = "Guilherme"; //String
+const nome2 = `Guilherme`; // String
+
+const num1 = 10; //number
+const num2 = 10.52; //number
+
+let nomeAluno; // Váriavel não inicializada -> undefined -> Não aponta pra lugar nenhum na memória
+
+const sobrenomeAluno = null; // Nulo -> Não aponta pra local nenhum na memória
+
+const aprovado = true; // boolean -> true or false
+const reprovado = false;
+
