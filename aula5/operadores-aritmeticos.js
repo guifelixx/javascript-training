01dac707c3068eb97914a1d5c49bc900184ef707
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-06-17 04:52:41 -0300

    operadores arit e atribu

---
+++ aula5/operadores-aritmeticos.js
@@ -0,0 +1,55 @@
+/* 
+Operadores aritméticos
++ Adição / Concatenação
+- Subtração
+/ Divisão
+* e ** Multiplicação e potenciação
+% resto da divisão
+*/
+
+//+ Adição / Concatenação
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
+//- Subtração
+const num3 = 5;
+const num4 = 10;
+console.log(num3 - num4);
+
+/// Divisão
+const num5 = 10;
+const num6= 5;
+console.log(num5 / num6);
+
+//Potenciação
+const num7 = 2;
+const num8= 10;
+console.log(num7 ** num8);
+
+//Multiplicação
+const num9 = 2;
+const num10= 10;
+console.log(num9 * num10);
+
+//% resto da divisão
+const num11 = 10;
+const num12= 3;
+console.log(num11 % num12);
+
+//Incremento ++
+let contador = 1;
+contador++; // 2
+contador++; // 3
+contador++; // 4
+contador++; // 5
+console.log(contador);
+
+// Decremento --
+let contador1 = 10;
+contador1--; // 9
+contador1--; // 8
+contador1--; // 7
+contador1--; // 6
+contador1--; // 5
+console.log(contador1);
---
+++ aula5/operadores-atribuicao.js
@@ -0,0 +1,25 @@
+//Operadores de atribuição
+// Pode ser feito com qulquer operador aritmético
+let contador = 0; // contador = contador + 50
+contador += 2; 
+contador += 2;
+contador += 2;
+console.log(contador );
+
+//NaN - Not a number
+const num1 = 100;
+const num2 = 'Guilherme';
+console.log(num1 + num2);
+
+//parseInt, parseFloat e number
+const num3 = 10;
+const num4 = parseInt('5');
+console.log(num3 + num4);
+
+const num5 = 10;
+const num6 = parseFloat('5.5');
+console.log(num5 + num6);
+
+const num7 = 10;
+const num8 = Number('50');
+console.log(num7 + num8);
