60e13a6a1e58c5305d588ab26467d45716b1f5b8
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-06-18 04:51:35 -0300

    exercicios

---
+++ aula6/js/script.js
@@ -0,0 +1,5 @@
+window.alert('Olá mundo!'); // Função que faz um alerta no navegador -> undefined
+
+window.confirm('Deseja realmente apagar?'); //Função que retorna true or false "Ok" ou "cancelar"
+
+window.prompt('Digite seu nome:'); //Função que abre uma caixa de texto para digitar tipo de dados, podendo guardar emm variaveis
