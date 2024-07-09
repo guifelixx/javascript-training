0f1538a9aebe6565bec6aecfcdeaebccc0b3bda0
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-07-09 19:27:37 -0300

    Exercicio concluido

--- exercicios/exercicio6/script.js
+++ exercicios/exercicio6/script.js
@@ -1,6 +1,6 @@
 //Matar o envio do formulario porque vamos usar para nós mesmos
 function meuEscopo () {
-    const form = document.querySelector('.form'); //Selecionar elementos como seletores do CSS
+    const form = document.querySelector('.form'); //Selecionei elementos e atribui a uma Const
     const resultado = document.querySelector('.resultado') //Selecionar div que vai exibir os resultados
 
     const pessoas = [];
@@ -12,6 +12,17 @@ function meuEscopo () {
         const sobrenome = form.querySelector('.sobrenome')
         const peso = form.querySelector('.peso')
         const altura = form.querySelector('.altura')
+
+        pessoas.push({ //Adicionar objeto a lista "pessoas"
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        }) 
+
+        console.log(pessoas); //Teste pra ver se a lista está armazenando corretamente
+        
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}<br /></p>`; //Exibir minha div com os resultados
     }
 
     form.addEventListener('submit', recebeEventoForm);
