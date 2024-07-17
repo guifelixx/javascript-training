3518bc916074d18b0488406c3a07cca752ec54e7
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-07-17 04:59:14 -0300

    aprendendo mais sobre if-else e criando modelo exercicio

--- estruturas-condicionais/script.js
+++ estruturas-condicionais/script.js
@@ -25,3 +25,18 @@ if (hora >= 0 && hora <= 11) { // Se hora for maior ou igual a 0 AND menor e igu
 } else { // Se hora estiver fora de 0 a 23 ele vai imprimir "Olá", porque ele não sabe a hora.
     console.log('Ola');
 }
+
+//Treino de lógica
+
+const numero = 6;
+
+if (numero >= 0 && numero <= 5) { // IF = Se isso ocorrer. faça isso:
+    console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) { //ELSE IF = Se não, faça isso:
+    console.log('O número está entre 6 e 8');
+} else if (numero >= 9 && numero <= 11) { //ELSE IF = Se não, faça isso:
+    console.log('O número está entre 9 e 11');
+} else { // ELSE = Se não ocorrer, faça isso:
+    console.log('O número NÃO está entre 0 e 11.')
+}
+
