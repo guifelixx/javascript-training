c902aa672a432d5d28df0dfa453521cb46f26758
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-07-19 04:57:58 -0300

    quase acabando exercicio

--- exercicios/exercicio7/assets/js/script.js
+++ exercicios/exercicio7/assets/js/script.js
@@ -2,18 +2,33 @@ function calculaImc () {
     const form = document.querySelector('.form')
     const resultado = document.querySelector('.resultado')
 
+    
     function calculo (evento) {
-        evento.preventDefalut();
+        evento.preventDefault();
 
-        const peso = form.querySelector('#input-peso')
-        const altura = form.querySelector('#input-altura')
+        const peso = form.querySelector('.input-peso')
+        const altura = form.querySelector('.input-altura')
 
-        const imc = peso.value / (altura.value ** 2)
-
-        console.log(imc);
+        const imc = (peso.value / (altura.value ** 2)).toFixed(2)
+        
+        if (imc < 18.50) {
+            resultado.innerHTML + `Seu IMC é ${imc} (Abaixo do peso)`;
+        } else if (imc >= 18.50 && imc <= 24.90) {
+            resultado.innerHTML + `Seu IMC é ${imc} (Peso normal)`;
+        } else if (imc >= 25 && imc <= 29.90) {
+            resultado.innerHTML + `Seu IMC é ${imc} (Sobrepeso)`;
+        } else if (imc >= 30 && imc <= 34.90) {
+            resultado.innerHTML + `Seu IMC é ${imc} (Obesidade grau 1)`;
+        } else if (imc >= 35 && 39.90) {
+            resultado.innerHTML + `Seu IMC é ${imc} (Obesidade grau 2)`;
+        } else if (imc > 40) {
+            resultado.innerHTML + `Seu IMC é ${imc} (Obesidade grau 3)`;
+        } else {
+            resultado.innerHTML + 'Valores inválidos';
+        }
     }
 
-    form.addEventListener('submit', calculo)
+    form.addEventListener('submit', calculo);
 }
 
 calculaImc();
