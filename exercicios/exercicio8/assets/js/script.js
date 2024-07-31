0cae0eb20f6a0edc907f465a83da7652114e8414
guifelixx <guilhermefelixpascoal@hotmail.com>
2024-07-31 04:57:04 -0300

    exercicio finalizado

--- exercicios/exercicio8/assets/js/script.js
+++ exercicios/exercicio8/assets/js/script.js
@@ -2,34 +2,46 @@ const date = new Date();
 let dayWeek = date.getDay();
 let month = date.getMonth();
 
+function formataData () { //Função que formata a forma como a data vai ser exibida na web
+    const diaSemana = dayFormat();
+    const mes = monthFormat();
+
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minute = date.getMinutes();
+
+    return `${diaSemana}, ${day} de ${mes} de ${year} ${hours}:${minute}`
+}
+
 //Uma função que formata o "day" para texto.
-function addData (dayWeek) {
+function dayFormat () {
     let dayWeekText;
 
     //Formatar dia da semana
     switch (dayWeek) {
         case 0:
-            dayWeekText = 'Domingo'
+            dayWeekText = 'domingo'
             return dayWeekText;
 
         case 1:
-            dayWeekText = 'Segunda-feira'
+            dayWeekText = 'segunda-feira'
             return dayWeekText;
 
         case 2:
-            dayWeekText = 'Terça-feira'
+            dayWeekText = 'terça-feira'
             return dayWeekText;
 
         case 3:
-            dayWeekText = 'Quarta-feira'
+            dayWeekText = 'quarta-feira'
             return dayWeekText;
 
         case 4:
-            dayWeekText = 'Quinta-feira'
+            dayWeekText = 'quinta-feira'
             return dayWeekText;
 
         case 5:
-            dayWeekText = 'Sexta-feira'
+            dayWeekText = 'sexta-feira'
             return dayWeekText;
 
         case 6:
@@ -40,70 +52,72 @@ function addData (dayWeek) {
             dayWeekText = ''
             return dayWeekText;
     }
+}
 
-    //Formatar o mês para "string"
-    function monthFormat (month) {
-        let monthText;
-        
-        switch (month) {
-            case 0:
-                monthText = 'Janeiro'
-                return monthText;
+//Formatar o mês para texto
+function monthFormat () {
+    let monthText;
     
-            case 1:
-                monthText = 'Fevereiro'
-                return monthText;
-    
-            case 2:
-                monthText = 'Março'
-                return monthText;
-                
-            case 3:
-                monthText = 'Maio'
-                return monthText;
-    
-            case 4:
-                monthText = 'Junho'
-                return monthText;
-    
-            case 5:
-                monthText = 'Julho'
-                return monthText;
-    
-            case 6:
-                monthText = 'Agosto'
-                return monthText;
-    
-            case 7:
-                monthText = 'Setembro'
-                return monthText;
-    
-            case 8:
-                monthText = 'Outubro'
-                return monthText;
-    
-            case 9:
-                monthText = 'Novembro'
-                return monthText;
-    
-            case 10:
-                monthText = 'Dezembro'
-                return monthText;
-    
-            default:
-                monthText = ''
-                return monthText;
-        }
+    switch (month) {
+        case 0:
+            monthText = 'Janeiro'
+            return monthText;
+
+        case 1:
+            monthText = 'Fevereiro'
+            return monthText;
+
+        case 2:
+            monthText = 'Março'
+            return monthText;
+
+        case 3:
+            monthText = 'Abril'
+            return monthText;
+            
+        case 4:
+            monthText = 'Maio'
+            return monthText;
+
+        case 5:
+            monthText = 'Junho'
+            return monthText;
+
+        case 6:
+            monthText = 'Julho'
+            return monthText;
+
+        case 7:
+            monthText = 'Agosto'
+            return monthText;
+
+        case 8:
+            monthText = 'Setembro'
+            return monthText;
+
+        case 9:
+            monthText = 'Outubro'
+            return monthText;
+
+        case 10:
+            monthText = 'Novembro'
+            return monthText;
+
+        case 11:
+            monthText = 'Dezembro'
+            return monthText;
+
+        default:
+            monthText = ''
+            return monthText;
     }
+}
 
-    function formataData (dayWeek, month) {
-        const day = zeroAEsquerda(date.getDate());
-        const year = zeroAEsquerda(date.getFullYear());
-        const hours = zeroAEsquerda(date.getHours());
-        const minute = zeroAEsquerda(date.getMinutes());
+function addData () {
+    const formatDate = formataData();
+    const resultado = document.querySelector('.result')
 
-        return `${dayWeek}, ${day} de ${month} de ${year} ${hours}:${minute}`
-    }
+    resultado.innerHTML = `<p>${formatDate}</p>`;
 }
 
 addData();
